102186d1c7a33c368cac96a03c0a070122d09906
Erich Meissner <meisserich@gmail.com>
2020-04-09 01:11:32 -0400

    iterative method of preorder traversal binary tree

--- BinaryTreePreorderTraversal.java
+++ BinaryTreePreorderTraversal.java
@@ -2,6 +2,7 @@ package techQuestions;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 public class BinaryTreePreorderTraversal {
 
@@ -36,15 +37,31 @@ public class BinaryTreePreorderTraversal {
 	public static List<Integer> preorderTraversalIterative(TreeNode root) {
 		if (root == null) {
 			return new ArrayList<Integer>();
-		}		
-		list.add(root.val);
-		if (root.left != null) {
-			preorderTraversal(root.left);
-		}
-		if (root.right != null) {
-			preorderTraversal(root.right);
 		}
 		
+		// the recursive method is trivial. what about iterative?
+		// we use a stack data structure. pop the current node into the list,
+		// and then add its two child nodes
+		
+		Stack<TreeNode> stack = new Stack<>();
+		
+		stack.add(root);
+		// stack initialized with the root TreeNode
+		while (!stack.isEmpty()) {
+			TreeNode currNode = stack.pop();
+			list.add(currNode.val);
+			// add right node to stack first since it will get pushed down
+			// if there exists a left node
+			if (currNode.right != null) {
+				stack.add(currNode.right);
+			}
+			// now add left node so that it will be at the top of the stack
+			if (currNode.left != null) {
+				stack.add(currNode.left);
+			}
+			// stack, if the tree is not only one root node, should have more elements
+		}
+ 		
 		return list;
 	}
 
