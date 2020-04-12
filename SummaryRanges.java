65826a3de62d23cd5a28c09bfa677e2014ef7edc
Erich Meissner <erich.meissner@gatech.edu>
2020-04-11 22:10:30 -0400

    another attempt at summary ranges

--- SummaryRanges.java
+++ SummaryRanges.java
@@ -1,4 +1,16 @@
 package techQuestions;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class SummaryRanges {
+    public static void main(String[] args) {
+        int[] nums = {0,1,2,4,5,7};
+        List<Integer> output = summaryRanges(nums);
+    }
+
+    public static List<Integer> summaryRanges(int[] nums) {
+        return new ArrayList<Integer>();
+    }
+
 }
