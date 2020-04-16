4b389b3dd869873d3c3c7cffceb8133bd52f265a
Erich Meissner <meisserich@gmail.com>
2020-04-16 01:41:43 -0400

    using a custom sort and min heap to determine meeting rooms

--- MeetingRooms2.java
+++ MeetingRooms2.java
@@ -1,41 +1,45 @@
 package techQuestions;
 
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
 public class MeetingRooms2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int[][] intervals = {{6,15},{13,20},{6,17}};
+		int[][] intervals = {{11,30},{10,20},{3,19},{8,12},{2,7},{1,10}};
 		
 		System.out.println(minMeetingRooms(intervals));
 	}
 
 	public static int minMeetingRooms(int[][] intervals) {
-		
-		int requiredRooms = 1;
-		int newRequirement = 1;
-		if (intervals == null || intervals.length == 0) {
+		Arrays.sort(intervals, new Comparator<int[]>() {
+			@Override
+			public int compare(int[] o1, int[] o2) {
+				return o1[0] - o2[0];
+			}
+		});
+
+		System.out.println(Arrays.deepToString(intervals));
+
+		PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+
+		if (intervals.length == 0) {
 			return 0;
 		}
-		
-		for (int i = 0; i < intervals.length; i++) {
-			newRequirement = 1;
-			for (int j = 0; j < intervals.length; j++) {
-				if (i == j) {
-					continue;
-				}
-				// if start is within start and end times OR end is within start and end times
-				if ((intervals[j][0] >= intervals[i][0] && intervals[j][0] <= intervals[i][1])
-						|| (intervals[j][1] >= intervals[i][0] && intervals[j][1] <= intervals[i][1])) {
-					System.out.println("j is: " + j + ", i is: " + i);
-					newRequirement = newRequirement + 1;
-					System.out.println(newRequirement);
-				}
+		// add first end time to minHeap
+		minHeap.add(intervals[0][1]);
+		for (int i = 1; i < intervals.length; i++) {
+			if (intervals[i][0] < minHeap.peek()) {
+				minHeap.add(intervals[i][1]);
+			} else {
+				minHeap.remove();
+				minHeap.add(intervals[i][1]);
 			}
-			System.out.println(newRequirement);
-			requiredRooms = Math.max(newRequirement, requiredRooms);
 		}
-		
-		
-		return requiredRooms;
+
+		return minHeap.size();
+
     }
 }
--- ProductArrayExceptSelf.java
+++ ProductArrayExceptSelf.java
@@ -1,9 +1,50 @@
 package techQuestions;
 
+import java.util.Arrays;
+
 public class ProductArrayExceptSelf {
-    public void main(String[] args) {
-        int[] nums = {1,2,3,4};
-        System.out.println();
+    public static void main(String[] args) {
+        int[] nums = {4,5,1,8,2};
+        System.out.println(Arrays.toString(productExceptSelf(nums)));
     }
+
+    public static int[] productExceptSelf(int[] nums) {
+        // answer with 0(N) space complexity where N is
+        // the number of elements in nums array
+//        int[] L = new int[nums.length],
+//                R = new int[nums.length];
+//        L[0] = 1;
+//        R[nums.length - 1] = 1;
+//        for (int i = 1; i < nums.length; i++) {
+//            L[i] = L[i-1] * nums[i-1];
+//        }
+//        for (int i = nums.length-2; i >= 0; i--) {
+//            R[i] = R[i+1] * nums[i+1];
+//        }
+
+        // what about O(1) space complexity?? don't
+        // allocate these extra arrays. Output/answer
+        // array doesn't count towards space
+        // complexity
+        int[] answer = new int[nums.length];
+        // initialize answer array with prod of left
+        answer[0] = 1;
+        for (int i = 1; i < nums.length; i++) {
+            answer[i] = answer[i-1] * nums[i-1];
+        }
+
+        int R = 1;
+        for (int i = nums.length - 2; i >= 0; i--) {
+            R = R * nums[i+1];
+            answer[i] = answer[i] * R;
+        }
+
+//        System.out.println(Arrays.toString(R) +
+//                " " + Arrays.toString(L));
+//        for (int i = 0; i < nums.length; i++) {
+//            nums[i] = L[i] * R[i];
+//        }
+        return answer;
+    };
 
 }
