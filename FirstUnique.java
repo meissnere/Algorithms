a0e26c1e9cb663d35a77de9faf70ec633a2649ff
Erich Meissner <erich.meissner@gatech.edu>
2020-04-29 21:58:56 -0400

    optimized to O(1) runtime

--- FirstUnique.java
+++ FirstUnique.java
@@ -1,54 +1,42 @@
 package techQuestions;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 public class FirstUnique {
 
-    public static List<Integer> list = new ArrayList<Integer>();
-    public Set<Integer> set = new HashSet<Integer>();
+    public Set<Integer> allAdded = new HashSet<Integer>();
+    public Set<Integer> uniqueOnly = new LinkedHashSet<Integer>();
 
     public FirstUnique(int[] nums) {
         for (int element : nums) {
-            if (list.contains(element)) {
-                list.remove(list.indexOf(element));
-                set.add(element);
-                continue;
-            } else if (set.contains(element)) {
-                continue;
-            }
-            list.add(element);
+            add(element);
         }
     }
 
     public int showFirstUnique() {
-        if (list.isEmpty()) {
+        // our unique set is completely empty!
+        if (uniqueOnly.isEmpty()) {
             return -1;
         }
-        return list.get(0);
+        return uniqueOnly.iterator().next();
     }
 
     public void add(int value) {
-        if (list.contains(value)) {
-            list.remove(list.indexOf(value));
-            return;
-        } else if (set.contains(value)) {
-            return;
+        // we know that an add on a set will fail if
+        // the element already exists!
+        if (allAdded.add(value)) {
+            uniqueOnly.add(value);
+        } else {
+            // this value is in allAdd! remove it from unique set
+            uniqueOnly.remove(value);
         }
-        list.add(value);
     }
 
     public static void main(String[] args) {
-        int[] nums = {7,7,7,7,7,7};
-        FirstUnique obj = new FirstUnique(nums);
-        obj.add(7);
-        obj.add(3);
-        obj.add(3);
-        obj.add(7);
-        obj.add(17);
-        System.out.println(obj.showFirstUnique());
+        // let me test this theory of truthiness for a set.add() ...
+//        Set<Integer> test = new HashSet<Integer>();
+//        System.out.println(test.add(5));
+//        System.out.println(test.add(5));
     }
 }
 
