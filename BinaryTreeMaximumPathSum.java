e8fdf7e2353db6b99a07ea5e88a40d3495e3ff6a
Erich Meissner <erich.meissner@gatech.edu>
2020-05-04 00:33:29 -0400

    recursive approach to finding max path

---
+++ BinaryTreeMaximumPathSum.java
@@ -0,0 +1,58 @@
+package techQuestions;
+
+import com.sun.source.tree.Tree;
+
+public class BinaryTreeMaximumPathSum {
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    static int max_sum = Integer.MIN_VALUE;
+
+    public static int max_gain(TreeNode node) {
+        if (node == null) return 0;
+
+        // max sum on the left and right sub-trees of node
+        int left_gain = Math.max(max_gain(node.left), 0);
+        int right_gain = Math.max(max_gain(node.right), 0);
+
+        // the price to start a new path where `node` is a highest node
+        int price_newpath = node.val + left_gain + right_gain;
+
+        // update max_sum if it's better to start a new path
+        max_sum = Math.max(max_sum, price_newpath);
+
+        // for recursion :
+        // return the max gain if continue the same path
+        return node.val + Math.max(left_gain, right_gain);
+    }
+
+    public static int maxPathSum(TreeNode root) {
+        max_gain(root);
+        return max_sum;
+    }
+
+    public static void main(String[] args) {
+        TreeNode tree = new TreeNode(-10);
+        tree.left = new TreeNode(9);
+        tree.right = new TreeNode(20);
+        tree.right.left = new TreeNode(15);
+        tree.right.right = new TreeNode(7);
+        System.out.println(maxPathSum(tree));
+    }
+}
