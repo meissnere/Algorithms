07bbbfc456304cd41bffc8355a604b39ac7009df
Erich Meissner <meisserich@gmail.com>
2020-05-08 21:24:47 -0400

    slope checks at each point

--- CheckStraightLine.java
+++ CheckStraightLine.java
@@ -6,10 +6,40 @@ public class CheckStraightLine {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int[][] coordinates = {{-4,-3},{1,0},{3,-1},{0,-1},{-5,2}};
+		int[][] coordinates = {{1,2},{2,3},{3,4},{4,5},{5,6},{6,7}};
+		int[][] secondCoords = {{1,1},{2,2},{3,4},{4,5},{5,6},{7,7}};
 		
-		System.out.println(checkStraightLine(coordinates));
+		System.out.println(checkStraightLineNew(secondCoords));
 	}
+
+	public static boolean checkStraightLineNew(int[][] coordinates) {
+		// if rise over run for each pair of coordinates
+		// is not equal, then we know this is not a straight
+		// line!
+		int x_base = coordinates[0][0];
+		int y_base = coordinates[0][1];
+		float slope = (float) 0.0;
+		if (coordinates[1][0] - x_base != 0) {
+			slope = (float)(coordinates[1][1] - y_base) / (coordinates[1][0] - x_base);
+		} else {
+			return false;
+		}
+//		System.out.println(slope);
+//		System.out.println(Arrays.deepToString(coordinates));
+		for (int i = 2; i < coordinates.length; i++) {
+			if (coordinates[i][0] - x_base != 0) {
+				float currSlope = (float)(coordinates[i][1] - y_base) / (coordinates[i][0] - x_base);
+				if (currSlope != slope) {
+					return false;
+				}
+			} else {
+				return false;
+			}
+
+		}
+		return true;
+	}
+
 	
 	public static boolean checkStraightLine(int[][] coordinates) {
 		System.out.println(Arrays.toString(coordinates[0]));
